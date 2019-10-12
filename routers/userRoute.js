0106975852974fe52dc5e85983dd1710a2e8ccd8
Amanat Juwel <amanatjuwel@gmail.com>
2019-10-12 15:00:53 +0600

    user route added

---
+++ routers/userRoute.js
@@ -0,0 +1,15 @@
+const router = require('express').Router()
+
+// Registration Route
+// localhost:4000/api/users/register
+router.post('/register',(req,res)=> {
+
+})
+
+// Login Route
+// localhost:4000/api/users/login
+router.post('/login',(req,res)=> {
+	
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -4,12 +4,16 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
+const userRouter = require('./routers/userRoute')
+
 const app = express()
 app.use(morgan('dev'))
 app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+app.use('api/users/', userRouter)
 
 app.get('/', (req, res) => {
 	res.json({
