44068dfed00ebb0a924622ee16ebb450c68ada51
Amanat Juwel <amanatjuwel@gmail.com>
2019-10-12 14:09:50 +0600

    mongoose added

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express()
+app.use(morgan('dev'))
+app.use(cors())
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
 	res.json({
 		message: 'Welcome To Our Application'
@@ -9,5 +19,10 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT, ()=> {
-	console.log('SERVER is RUNNING ON PORT ${PORT}')
+	console.log(`SERVER is RUNNING ON PORT ${PORT}`)
+	mongoose.connect('mongodb://localhost/money-management-app',
+		{ userNewUlrParser: true},
+		() =>{
+			console.log('Database Connected...')
+		})
 })
