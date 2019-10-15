c4972186a0ad4e148481b577f59e87a75d884180
Amanat Juwel <amanatjuwel@gmail.com>
2019-10-15 12:36:19 +0600

    user registration with bcrypt and mongod

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,7 @@
+const bcrypt = require('bcrypt')
 const registerValidator = require('../validator/registerValidator')
+const User = require('../model/User')
+
 // login controller 
 module.exports = {
 	login(req, res) {
@@ -9,15 +12,57 @@ module.exports = {
 		})
 	},
 	register(req, res) {
-		let { name, email, password, confirmPassword } = req.body
+        let { name, email, password, confirmPassword } = req.body
         let validate = registerValidator({ name, email, password, confirmPassword })
         
         if (!validate.isValid) {
             return res.status(400).json(validate.error)
         } else {
-        	return res.status(200).json({
-        		message: 'Everything is ok'
-        	})
+        	User.findOne({email})
+        		.then(user => {
+        			if(user){
+        				return res.status(400).json({
+        					message: 'Email Already Exist'
+        				})
+        			}
+
+        			bcrypt.hash(password, 11, (err, hash) => {
+        				if(err){
+        					return res.status(500).json({
+        						message: 'Server Error Occured'
+        					})
+        				}
+
+        				let user = new User({
+	        				name,
+	        				email,
+	        				password : hash
+	        			})
+
+        				user.save()
+        					.then(user => {
+        						return res.status(201).json({
+        							message: 'User Created Successfully', 
+        							user
+        						})
+        					})
+        					.catch(error => {
+			        			console.log(error)
+			        			res.status(500).json({
+					                message: 'Server Error Occurred'
+					            })
+			        		})
+
+        			})
+
+        		})
+        		.catch(error => {
+        			console.log(error)
+        			res.status(500).json({
+		                message: 'Server Error Occurred'
+		            })
+        		})
+            
         }
-	}
+    },
 }
