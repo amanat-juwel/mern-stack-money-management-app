37d5bdfc9f62a10314d3f8337443eecc20217318
Amanat Juwel <amanatjuwel@gmail.com>
2019-10-17 17:17:36 +0600

    transaction update,delete and dynamic navbar added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Home from '../pages/Home'
 import Login from '../pages/Login'
 import Register from '../pages/Register'
+import Dashboard from '../pages/Dashboard'
+import Navigation from './navigation/Navigation'
 
-function App() {
-  return (
-  	<BrowserRouter>
-    <div className="container">
-      <Switch>
-      	<Route path="/" exact component={Home} />
-      	<Route path="/login" exact component={Login} />
-      	<Route path="/register" exact component={Register} />
-      </Switch>
-    </div>
-    </BrowserRouter>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="container">
+          <Navigation />
+        <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/login' component={Login} />
+            <Route path='/register' component={Register} />
+            <Route path='/dashboard' component={Dashboard} />
+        </Switch>
+      </div>   
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,113 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { loadTransactions, removeTransaction } from '../store/actions/transactionActions'
+import CreateTransaction from '../components/transaction/CrerateTransaction'
+import UpdateTransaction from '../components/transaction/UpdateTransaction'
+
+
+class Dashboard extends React.Component {
+
+    state = {
+        createModalOpen: false,
+        updateModalOpen: false,
+        id: ''
+    }
+
+    openCreateModal = () => {
+        this.setState({
+            createModalOpen: true
+        })
+    }
+
+    closeCreateModal = () => {
+        this.setState({
+            createModalOpen: false
+        })
+    }
+
+    openUpdateModal = (id) => {
+        this.setState({
+            updateModalOpen: true,
+            id
+        })
+    }
+
+    closeUpdateModal = () => {
+        this.setState({
+            updateModalOpen: false,
+            id: ''
+        })
+    }
+
+
+    componentDidMount() {
+        this.props.loadTransactions()
+    }
+
+    render() {
+        let {auth, transactions} = this.props
+        return (
+            <div className="row">
+                <div className="col-md-8 offset-md-2">
+                    <h1>Welcome {auth.user.name} </h1>
+                    <p>You Email is {auth.user.email} </p>
+
+                    <button
+                        className='btn btn-primary'
+                        onClick={this.openCreateModal}
+                    >
+                        Create New Transaction
+                    </button>
+                    <CreateTransaction
+                        isOpen={this.state.createModalOpen}
+                        close={this.closeCreateModal}
+                    />
+                    <br />
+                    <h1>Transactions: </h1>
+                    {transactions.length > 0 ? <ul className='list-group'>
+                        {
+                            transactions.map(transaction => (
+                                <li
+                                    key={transaction._id}
+                                    className='list-group-item'>
+                                    <p>Type: {transaction.type}</p>
+                                    <p>Amount: {transaction.amount}</p>
+
+                                    {
+                                        this.state.id === transaction._id ?
+                                        <UpdateTransaction
+                                            isOpen={this.state.updateModalOpen}
+                                            close={this.closeUpdateModal}
+                                            transaction={transaction}
+                                        /> :  null
+                                    }
+
+                                    <button
+                                        className='btn btn-success'
+                                        onClick={()=>this.openUpdateModal(transaction._id)}
+                                    >
+                                    Update
+                                    </button>
+
+                                    <button
+                                        className='btn btn-danger'
+                                        onClick={()=>this.props.removeTransaction(transaction._id)}
+                                    >
+                                    Remove
+                                    </button>
+                                </li>
+                            ))
+                        }
+                    </ul> : <p>There is no transaction</p>}
+                </div>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    auth: state.auth,
+    transactions: state.transactions
+})
+
+export default connect(mapStateToProps, { loadTransactions, removeTransaction })(Dashboard)
